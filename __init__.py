0297fa2cceb2c35326d79d600f0599a13461c2eb
Norman <norman.marlier@gmail.com>
2020-06-16 16:40:20 +0200

    Module - Need to add relative path for kinect urdf file

--- __init__.py
+++ __init__.py
@@ -4,4 +4,8 @@ Created on Wed Jun 10 18:28:35 2020
 
 @author: norman marlier
 """
+from .perlin_noise import PerlinNoise
 
+# Kinect urdf
+# TODO: change this !
+KINECT_URDF = "C:/Users/norma/Documents/phd/Pybullet/kinect_pybullet/kinect_urdf.urdf"
